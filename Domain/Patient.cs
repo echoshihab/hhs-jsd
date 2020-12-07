1f2336054aa200c8bd478f32eb3f4bbe4fee2ff0
shihab <echoshihab@gmail.com>
2020-12-07 15:24:11 -0500

    first migration - add Patient and AppUser to db

---
+++ Domain/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Patient
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public long HealthCardNumber { get; set; }
+        public string VersionCode { get; set; }
+    }
+
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
